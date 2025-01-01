ad5f6ae200a9e154e8195f082a691d445ee50237
yond5413 <yd2696@columbia.edu>
2025-01-01 18:23:19 -0500

    updates to next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export', // Enable static export
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
+  basePath: '/<repository-name>', // Replace <repository-name> with your repo name if deploying to a subpath
+  assetPrefix: '/<repository-name>', // Matches basePath for assets
+};
+
+export default nextConfig;
