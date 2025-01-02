136f9dfdf27bf592d10c6b8f78a902a5d6bf4d5d
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-01-01 19:05:26 -0500

    Update next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -4,8 +4,7 @@ const nextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
-  basePath: '/<repository-name>', // Replace <repository-name> with your repo name if deploying to a subpath
-  assetPrefix: '/<repository-name>', // Matches basePath for assets
+  basePath: 'portfolio-site', 
 };
 
 export default nextConfig;
